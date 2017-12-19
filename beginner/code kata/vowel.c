bece6a7c4b3b63e48b35c9b3eced0554bab1e1e3
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2017-12-19 12:23:27 +0530

    Update vowel.c

--- beginner/code kata/vowel.c
+++ beginner/code kata/vowel.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	char a[5]={'a','e','i','o','u'};
+
 	char b;
-	int count=0;
+
 	scanf("%c",&b);
-	for(int i=0;i<5;i++)
-	{
-		if(b==a[i])
-		{
-		count++;
-		}
-	}
-	if(count>0)
-	printf("vowel");
-	else
-	printf("Consonant");
+	
+		(b=='a'||b=='e'||b=='i'||b=='o'||b=='u'||b=='A'||b=='E'||b=='O'||b=='I'||b=='U')?printf("vowel"):printf("Consonant");
+		
+
+	
+	
+	
 	return 0;
 }
