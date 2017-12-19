a2e9f9f3ae379a11390964865ce4829d5296f8ea
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2017-12-19 12:41:07 +0530

    Create largest_3_num.c

---
+++ beginner/code kata/largest_3_num.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+(a>b&&a>c)?printf("%d is greater",a):(b>c)?printf("%d is greater",b):printf("%d is greater",c);
+	return 0;
+}
