ed95f5c013833a4824050642515e6566563efcf9
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2017-12-20 07:19:55 -0800

    Update count_appearance_of_string.c

--- class assignment/count_appearance_of_string.c
+++ class assignment/count_appearance_of_string.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-#include<stdlib.h>
 #include<string.h>
 int main(void) {
 char s[20];
 
 scanf("%s",s);
 for(int i=0;i<strlen(s);i++)
-{int c=0,k=0;
-	for(int j=0;j<strlen(s);j++)
+{int c=1;
+	for(int j=i+1;j<strlen(s);j++)
 	{
 		if(s[i]==s[j])
 		{
 			c++;
+			if(c>1)
+			{
+				i++;
+			}
+			
+			
 		}
 	
 	}
