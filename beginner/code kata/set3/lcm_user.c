9e19a2be4c2ae56f4cbd228449722870fbce6650
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2017-12-20 09:36:18 -0800

    Update lcm_user.c

--- beginner/code kata/set3/lcm_user.c
+++ beginner/code kata/set3/lcm_user.c
@@ -3,12 +3,13 @@
 int main(void) {
 int a,b,lcm,gcd;
 scanf("%d %d",&a,&b);
-for(int i=2;i<=a&&i<=b;i++)
+int i=2;
+while(i<=a&&i<=b)
 {
 	if(a%i==0 && b%i==0)
 	{
 		gcd=i;
-	}
+	}i++;
 }
 lcm=(a*b)/gcd;
 printf("%d",lcm);
