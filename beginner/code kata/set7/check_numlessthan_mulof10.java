1cd358ce5a1288d6f65c2d9957a9cae1983e9f62
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-03-03 06:13:48 -0800

    Create check_numlessthan_mulof10.java

---
+++ beginner/code kata/set7/check_numlessthan_mulof10.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=10;i<10000;i+=10)
+		{
+			if(n<i)
+			{
+				System.out.print(i);
+				break;
+			}
+		}
+	
+	
+		
+	
+	
+		
+		
+		
+		
+		
+	}
+}
