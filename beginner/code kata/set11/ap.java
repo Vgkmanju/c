cc42677670747e878cd1242ff90f2084ac566aa6
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-03-04 00:34:45 -0800

    Create ap.java

---
+++ beginner/code kata/set11/ap.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner s=new Scanner(System.in);
+	    int a=s.nextInt();
+	    int b=s.nextInt();
+	    int c=s.nextInt();
+	    int sum=0;
+	    for(int i=0;i<c;i++)
+	    {
+	    	sum=sum+(a+(i*b));
+	    	
+	    }
+	    System.out.println(sum);
+	    
+	}
+}
