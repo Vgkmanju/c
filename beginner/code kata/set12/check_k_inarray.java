3a06b585ea45f809d6e0acf222f28e6fa9506fad
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-03-04 00:54:50 -0800

    Create check_k_inarray.java

---
+++ beginner/code kata/set12/check_k_inarray.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner s=new Scanner(System.in);
+	    int a=s.nextInt();
+	    int b=s.nextInt();
+	    int c[]=new int[a];
+	    int d=0;
+	    for(int i=0;i<a;i++)
+	    {
+	    	 c[i]=s.nextInt();
+	    }
+	    for(int j=0;j<a;j++)
+	    {
+	    	if(c[j]==b)
+	    	d=1;
+	    	
+	    }
+	    if(d==1)
+	    System.out.println("yes");
+	    else
+	    System.out.println("no");
+	    
+	}
+}
