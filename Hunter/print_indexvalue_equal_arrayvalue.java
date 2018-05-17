f4c8ca6291693880340d366fe0eb37b8373ade71
Vgkmanju <34670259+Vgkmanju@users.noreply.github.com>
2018-05-17 22:49:15 +0530

    Create print_indexvalue_equal_arrayvalue.java

---
+++ Hunter/print_indexvalue_equal_arrayvalue.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+ int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int q=0;q<n;q++)
+{
+	if(q==a[q])
+System.out.print(a[q]+" ");	
+}
+}
+}
